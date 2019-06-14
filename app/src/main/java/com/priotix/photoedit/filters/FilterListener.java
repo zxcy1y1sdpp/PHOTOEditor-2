5bb3d3302c6c53c7147120ae601b438502f7d60c
hovsep <hovsep.shakaryan@gmail.com>
2019-06-14 15:48:38 +0400

    Final project

---
+++ app/src/main/java/com/priotix/photoedit/filters/FilterListener.java
@@ -0,0 +1,7 @@
+package com.priotix.photoedit.filters;
+
+import ja.burhanrashid52.photoeditor.PhotoFilter;
+
+public interface FilterListener {
+    void onFilterSelected(PhotoFilter photoFilter);
+}
